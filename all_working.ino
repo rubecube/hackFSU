2d8d64f35bf878e11af5f18880898933f593adaa
Ruben <ruben@wc-dhcp94d146.eduroam.wireless.fsu.edu>
2018-03-03 14:10:37 -0500

    update all_working.ino

--- all_working.ino
+++ all_working.ino
@@ -179,12 +179,29 @@ void loop()
   while (lenghtValue < 64 && lenghtValue != 0){           /// check if the person is close enought
     //Serial.println("In the loop");
 
-      
+    lsm.read();  // ask it to read in the data 
+    // Get a new sensor event 
+    sensors_event_t a, m, g, temp;
 
-    gyroSensor();
+    lsm.getEvent(&a, &m, &g, &temp); 
+
+    Serial.print("Accel X: "); Serial.print(a.acceleration.x); Serial.print(" m/s^2");
+    Serial.print("\tY: "); Serial.print(a.acceleration.y);     Serial.print(" m/s^2 ");
+    Serial.print("\tZ: "); Serial.print(a.acceleration.z);     Serial.println(" m/s^2 ");
+  
+    Serial.print("Mag X: "); Serial.print(m.magnetic.x);   Serial.print(" gauss");
+    Serial.print("\tY: "); Serial.print(m.magnetic.y);     Serial.print(" gauss");
+    Serial.print("\tZ: "); Serial.print(m.magnetic.z);     Serial.println(" gauss");
+  
+    Serial.print("Gyro X: "); Serial.print(g.gyro.x);   Serial.print(" dps");
+    Serial.print("\tY: "); Serial.print(g.gyro.y);      Serial.print(" dps");
+    Serial.print("\tZ: "); Serial.print(g.gyro.z);      Serial.println(" dps");
+  
+    Serial.println();
+    delay(200);  
 
 
-
+    
 
 
     
@@ -199,9 +216,9 @@ void loop()
 void setupSensor()
 {
   // 1.) Set the accelerometer range
-  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
   //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
   //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
   
   // 2.) Set the magnetometer sensitivity
@@ -211,9 +228,9 @@ void setupSensor()
   //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
 
   // 3.) Setup the gyroscope
-  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
   //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
-  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
 }
 
 
@@ -237,10 +254,9 @@ int lenghtOfSonar(){
   
 }
 
-void gyroSensor(){
-  lsm.read();  /* ask it to read in the data */ 
-
-  /* Get a new sensor event */ 
+/*void gyroSensor(){
+  lsm.read();  // ask it to read in the data 
+  // Get a new sensor event 
   sensors_event_t a, m, g, temp;
 
   lsm.getEvent(&a, &m, &g, &temp); 
@@ -261,7 +277,7 @@ void gyroSensor(){
   delay(200);
 
   
-}
+}*/
 
 void buzz(int targetPin, long frequency, long length) {
     long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
